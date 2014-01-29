99be8454274d48bfdf0d644c5c684b3eb1d29943
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2014-01-29 08:10:40 -0500

    added a dynamic title to the home page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,6 +4,11 @@ describe "Static Pages" do
 
   	describe "Home page" do
 
+  		it "should have the correct title" do
+  			visit '/static_pages/home'
+  			expect(page).to have_title("DevNation Bank | Home")
+  		end
+
   		it "should have the content 'DevNation'" do
   			visit '/static_pages/home'
   			expect(page).to have_content('DevNation')
