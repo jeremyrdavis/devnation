9589dd1f5dedbf6666ae5a759ff99cf2371f15e2
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2014-02-04 21:28:39 -0500

    Updated page titles

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -6,8 +6,13 @@ describe "Static Pages" do
 
   		it "should have the correct title" do
   			visit '/static_pages/home'
-  			expect(page).to have_title("DevNation Bank | Home")
+  			expect(page).to have_title("DevNation Bank")
   		end
+
+      it "should not have a custom page title" do
+        visit '/static_pages/home'
+        expect(page).not_to have_title('| Home')
+      end
 
   		it "should have the content 'DevNation'" do
   			visit '/static_pages/home'
