b0fa2efec0e787d87b1d63f72228aecb130b7042
Jeremy Davis <jeremy.davis@redhat.com>
2014-02-22 08:30:00 -0500

    added validation

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,4 +8,24 @@ describe Customer do
 
 	it { should respond_to(:first_name)}
 	it { should respond_to(:last_name)}
+	it { should respond_to(:email)}
+
+	# validations
+	it { should be_valid }
+
+	describe "first_name is required" do
+		before { @customer.first_name = "" }
+		it { should_not be_valid }
+	end
+
+	describe "last_name is required" do
+		before { @customer.last_name = "" }
+		it { should_not be_valid }
+	end
+
+	describe "email is required" do
+		before { @customer.email = "" }
+		it { should_not be_valid }
+	end
+
 end
