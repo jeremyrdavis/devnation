7607ffecd2f201617a9f22919fea1234d7aa24e9
Jeremy Davis <jeremy.davis@redhat.com>
2014-02-28 20:57:52 -0500

    Finished Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,10 +5,10 @@ class Customer < ActiveRecord::Base
 
 	validates :first_name, presence: true
 	validates :last_name, presence: true
-	validates :password, length: { minimum: 6 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   	has_secure_password
 	has_many :accounts
+	validates :password, length: { minimum: 6 }
 end
