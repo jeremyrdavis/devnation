3c44bbefa7e2aef8068a783011bd68870316c484
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2014-03-12 19:11:38 -0400

    Added links to static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -26,6 +26,12 @@ describe "Static Pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+      it "should have the title 'DevNation Bank | Help'" do
+        visit '/static_pages/help'
+        expect(page).to have_title("DevNation Bank | Help")
+      end
+
   end
 
   describe "About page" do
@@ -34,6 +40,26 @@ describe "Static Pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About DevNation Bank')
     end
+
+    it "should have the title 'DevNation Bank | About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("DevNation Bank | About Us")
+    end
+
+  end
+
+  describe "Contact page" do
+    
+    it "should have the content, 'Contatct Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact DevNation Bank')
+    end
+
+    it "should have the title 'DevNation Bank'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("DevNation Bank | Contact Us")
+    end
+
   end
 
 end
