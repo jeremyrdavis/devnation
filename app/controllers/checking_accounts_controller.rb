febfb1b6af7629f67456aa16ae3472f368c31cba
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2014-04-03 16:31:33 -0400

    Added CheckingAccountController and pages spec

---
+++ app/controllers/checking_accounts_controller.rb
@@ -0,0 +1,22 @@
+class CheckingAccountsController < ApplicationController
+
+  # POST /customers
+  # POST /customers.json
+  def create
+    @customer = Customer.find(params[:customer_id])
+    @account = @customer.checking_accounts.create()
+
+    # return back to the Customer's page
+    respond_to do |format|
+      if @account
+        flash[:success] = "Welcome to DevNation Bank, #{@customer.first_name}!"
+        format.html { redirect_to @customer, notice: 'Checking account was successfully created!' }
+        format.json { render action: 'show', status: :created, location: @customer }
+      else
+        format.html { render action: 'show', location: @customer }
+        format.json { render json: @account.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+end
---
+++ spec/features/checking_accounts_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+
+describe "CheckingAccountsPages" do
+
+	subject{ page }
+	
+	# Create a customer for the tests
+	before { @customer = FactoryGirl.create(:customer)}
+
+	# Creating a CheckingAccount doesn't have an actual page.  The CheckingAccountsController
+	# creates the account and forwards back to the originating Customer page
+	describe "create from the Customer page" do
+		before { visit customer_path( @customer ) }
+        it { should have_button("Add Checking Account") }
+		
+		it "should create a CheckingAccount" do
+		  expect { click_button "Add Checking Account"}.to change(CheckingAccount, :count)
+		end
+	end
+
+    it "should have a welcome message after signup" do
+
+		visit customer_path( @customer )
+		click_button "Add Checking Account"
+		expect(page).to have_content("Checking account was successfully created!")
+
+    end
+
+end
