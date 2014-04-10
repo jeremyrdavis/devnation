c35de652697270e7ad7da550aa093ac3c0a7ca11
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2014-04-09 22:07:53 -0400

    Updated Transaction pages to show type

--- spec/features/transaction_pages_spec.rb
+++ spec/features/transaction_pages_spec.rb
@@ -16,5 +16,17 @@ describe "Transactions" do
 		
 	end
 
+	describe "all transactions page" do
+
+		before { visit("/transactions") }
+
+		it { should have_title("DevNation Bank Transactions") }
+		it { should have_content("From account") }
+		it { should have_content("To account") }
+		it { should have_content("Amount") }
+		it { should have_content("Type") }
+
+	end
+
 end
 
