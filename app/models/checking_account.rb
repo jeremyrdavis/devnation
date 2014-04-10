d431adc4ad12e6b5804ef75f25da51c47ccd3a6e
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2014-04-09 21:58:50 -0400

    Updated CheckingAccount/Transaction relationship

--- app/models/checking_account.rb
+++ app/models/checking_account.rb
@@ -1,11 +1,17 @@
 class CheckingAccount < ActiveRecord::Base
 	belongs_to :customer #, :class_name => "Customer", :foreign_key => "customer_id"
+	has_many :withdrawls, :class_name => "Transaction", :foreign_key => "from_account_id", :primary_key => "account_number"
+	has_many :deposits, :class_name => "Transaction", :foreign_key => "to_account_id", :primary_key => "account_number"
 	validates :customer_id, presence: true
 
-	#always downcase email address to ensure uniqueness
+	#always generate a random account number
 	before_save { self.account_number = generate_account_number }
 
 	def generate_account_number
 		self.account_number = "DNB" + Random.rand(12345678).to_s
+	end
+
+	def all_transactions
+		return Array.new(self.withdrawls + self.deposits)
 	end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ActiveRecord::Base
+	belongs_to :from_account, :class_name => "CheckingAccount"
+	belongs_to :to_account, :class_name => "CheckingAccount"
 
 	before_save :create_timestamp  
 
