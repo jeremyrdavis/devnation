7514793c27f1b1498ead7966b6ef695830fda993
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2014-04-09 22:44:34 -0400

    Updated factories for random Customer generation

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,18 +3,22 @@ require 'bigdecimal'
 FactoryGirl.define do
 
   factory :customer do
-    first_name "Jeremy"
+    first_name "Jeremy" 
     last_name "Davis"
     sequence(:email) { |n| "jeremy.davis#{n}@example.com" }
     password "redhat-14"
     password_confirmation "redhat-14"
 
 	  factory :customer_with_checking_account do
-      account_number = Random.rand(12345678)
+      email "#{Random.rand(123456)}@example.com"
 	  	after(:create) { |customer| 
         customer.checking_accounts.create(balance:1500)
       }
 	  	end
+
+    factory :customer_random do
+      email "#{Random.rand(123456)}@example.com"
+    end
   end
 
 
